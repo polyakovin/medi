c107965a40cdfbd9d304bd4ad1307693866a69d1
Igor Polyakov <web@polyakovin.ru>
2022-09-07 23:09:14 +0300

    stories added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [activeSection, setActiveSection] = useState(0);
+  const sections = [1,2,3,4,5,6,7,8,9];
+
+  const decrement = () => activeSection > 0 && setActiveSection(activeSection - 1);
+  const increment = () => activeSection < sections.length - 1 && setActiveSection(activeSection + 1);
+
+  useEffect(() => {
+    const handleKeypress = ({ key }: any) => {
+      if (key === 'ArrowLeft') {
+        decrement();
+      } else if (key === 'ArrowRight') {
+        increment();
+      }
+    };
+    window.addEventListener('keydown', handleKeypress);
+    return () => {
+      window.removeEventListener('keydown', handleKeypress);
+    };
+  }, [activeSection]);
+
+  console.log(activeSection);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="progress">
+      {sections.map((s, i) => <div key={i} className={`section${i <= activeSection ? ' active' : ''}`}></div>)}
     </div>
   );
 }
